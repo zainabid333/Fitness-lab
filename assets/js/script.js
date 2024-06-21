cb992bfd1f62b890fa08dfd4b6f58d9d674310ff
LuccaBertoldi <lbertoldi93@gmail.com>
2024-06-21 19:56:30 -0400

    did some styling and merged with the dropdown menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,4 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, options);
   });
-  instance.open();
+  
