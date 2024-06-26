2122d2d9f25521b6cc59c7da3831f2608ba017be
Aden Neal <adenocbenji@gmail.com>
2024-06-25 19:08:32 -0500

    added theme switcher

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, options);
   });
+
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('#theme');
+
+
+let mode = 'light';
+
+themeSwitcher.addEventListener('click', function () {
+
+  if (mode === 'dark') {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+  }
+  else {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+  }
+});
+
   
