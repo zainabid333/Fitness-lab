1f302e7475afbc5f253158891bb7162647615e5f
Aden Neal <adenocbenji@gmail.com>
2024-06-27 20:54:52 -0500

    added theme switcher

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,68 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.dropdown-trigger');
+//     var instances = elems.Dropdown.init(elems, options);
+//   });
 
-const themeSwitcher = document.querySelector('#theme-switcher');
-const container = document.querySelector('#theme');
-
-
-let mode = 'light';
-
-themeSwitcher.addEventListener('click', function () {
-
-  if (mode === 'dark') {
-    mode = 'light';
-    container.setAttribute('class', 'light');
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+      extend: {
+          colors: {
+              primary: '#3490dc',
+          }
+      }
   }
-  else {
-    mode = 'dark';
-    container.setAttribute('class', 'dark');
-  }
+}
+
+
+
+
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+
+        
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
 });
+
+
+
+
+
 
   
